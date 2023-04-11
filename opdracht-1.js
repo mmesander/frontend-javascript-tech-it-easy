c7eb46fafe280e11a8e550a34ff370dc97e08a47
mmesander <mark_mesander@hotmail.com>
2023-04-11 16:53:05 +0200

    Added opdracht-1a

---
+++ opdracht-1.js
@@ -0,0 +1,8 @@
+//--------------------Opdracht 1--------------------//
+//-------------Opdracht 1a-------------//
+
+const tvTypeNames = inventory.map((product)=>{
+    return product.name;
+})
+
+console.log(tvTypeNames)
