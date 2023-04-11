abfac9346d7a1a2b8a5b31be953301490c217b44
mmesander <mark_mesander@hotmail.com>
2023-04-11 19:18:42 +0200

    added opdracht 2b & 2c

--- opdracht-2.js
+++ opdracht-2.js
@@ -4,15 +4,25 @@
 let soldTvs = 0;
 
 for (let i = 0; i < inventory.length; i++) {
-    soldTvs = soldTvs + inventory[i].sold
+    soldTvs = soldTvs + inventory[i].sold;
 }
 
-console.log(soldTvs)
+console.log(soldTvs);
 
 //-------------Opdracht 2b-------------//
 
+const dashboardSold = document.getElementById('products-sold');
+dashboardSold.textContent = soldTvs;
+
 //-------------Opdracht 2c-------------//
 
+let boughtTvs = 0;
+
+for (let i = 0; i < inventory.length; i++) {
+    boughtTvs = boughtTvs + inventory[i].originalStock;
+}
+
+console.log(boughtTvs)
 //-------------Opdracht 2d-------------//
 
 //-------------Opdracht 2e-------------//
