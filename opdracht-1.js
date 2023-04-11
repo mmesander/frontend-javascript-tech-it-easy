32f0b506536741e3bc89bcb5b2336f22cdbd3dc6
mmesander <mark_mesander@hotmail.com>
2023-04-11 17:49:15 +0200

    Completed opdracht-1

--- opdracht-1.js
+++ opdracht-1.js
@@ -1,24 +1,29 @@
 //--------------------Opdracht 1--------------------//
 //-------------Opdracht 1a-------------//
 
-const tvTypeNames = inventory.map((product)=>{
+const tvTypeNames = inventory.map((product) => {
     return product.name;
-})
+});
 
 console.log(tvTypeNames);
 
 //-------------Opdracht 1b-------------//
 
-const soldOut = inventory.filter((product)=>{
+const soldOut = inventory.filter((product) => {
     return (product.originalStock - product.sold) === 0;
-})
+});
 
 console.log(soldOut);
 
 //-------------Opdracht 1c-------------//
 
-const tvWithAmbilight = inventory.filter((product)=>{
+const tvWithAmbilight = inventory.filter((product) => {
     return product.options.ambiLight === true;
-})
+});
 
-console.log(tvWithAmbilight)
+console.log(tvWithAmbilight);
+
+//-------------Opdracht 1d-------------//
+
+inventory.sort((a,b) => a.price - b.price);
+console.log(inventory);
